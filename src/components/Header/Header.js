19f6495f74082442b99f4aab49b09cd7ae8bc488
X932 <yusuf_020202@mail.ru>
2020-12-05 12:35:57 +0500

    supplemented eng, rus languages. Added animations.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import './Header.scss';
 import logo from './img/logoExample.png';
+import { useTranslation } from 'react-i18next';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import Fade from 'react-reveal/Fade';
 
 function Header() {
+    const { t, i18n } = useTranslation();
+
+    function handleLanguage(lang) {
+        i18n.changeLanguage(lang);
+    }
+
     return (
-        <nav className='fixed_nav_container'>
-            <AnchorLink href="#home" offset='80' className="logo">
-                <img src={logo} alt="LOGO" />
-            </AnchorLink>
-            <AnchorLink href="#about_us" offset='80' className="anchor">About Us</AnchorLink>
-            <AnchorLink href="" offset='80' className="anchor">Our Advantages</AnchorLink>
-            <AnchorLink href="" offset='80' className="anchor">Contacts</AnchorLink>
-        </nav>
+        <Fade top>
+            <nav className='fixed_nav_container'>
+                <AnchorLink href="#home" offset='80' className="logo">
+                    <img src={logo} alt="LOGO" />
+                </AnchorLink>
+                <AnchorLink href="#about_us" offset='80' className="anchor">{t('header.about_us')}</AnchorLink>
+                <AnchorLink href="#advantages" offset='80' className="anchor">{t('header.advantages')}</AnchorLink>
+                <AnchorLink href="" offset='80' className="anchor">{t('header.contacts')}</AnchorLink>
+
+                <div className="languages">
+                    <button onClick={() => handleLanguage('en')}>Eng</button>
+                    <button onClick={() => handleLanguage('ru')}>Rus</button>
+                </div>
+            </nav>
+        </Fade>
     )
 }
 
--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import './MainContent.scss';
 import iron from './img/iron.png';
+import Fade from 'react-reveal/Fade';
 
 function MainContent() {
     const { t } = useTranslation();
@@ -10,13 +11,17 @@ function MainContent() {
         <div>
             <div className='underheader' id='home'>
                 <div className='underheader_container'>
-                    <div className='underheader_left'>
-                        <h1 className='header'>{t('main_content.header')}</h1>
-                        <div>
-                            {t('main_content.content')}
-                        </div>    
-                    </div>
-                    <img className='underheader_img' src={iron} alt='IROn'></img>
+                    <Fade top delay={200}>
+                        <div className='underheader_left'>
+                            <h1 className='header'>{t('main_content.header')}</h1>
+                            <div>
+                                {t('main_content.content')}
+                            </div>    
+                        </div>
+                    </Fade>
+                    <Fade>
+                        <img className='underheader_img' src={iron} alt='IROn'></img>
+                    </Fade>
                 </div>
             </div>
         </div>
